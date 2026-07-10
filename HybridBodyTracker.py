0f71df94db3ace69d9010e6898ae13d0d0feec3f
Maciek <maciej.bialoleckifake@gmail.com>
2026-07-10 18:25:38 +0200

    HybridBodyTracker upgrades

--- HybridBodyTracker.py
+++ HybridBodyTracker.py
@@ -17,36 +17,9 @@ class HybridBodyTracker:
 
         self.candidate_pos = None
         self.candidate_count = 0
-    
-    def update(self, frame):
-        self.frame_idx += 1
-        run_detection = (self.state !="LOCKED") or (self.frame_idx % self.detect_every_n == 0)
-        if run_detection:
-            result = self.detect_fn(frame)
-            if result is None:
-                det_x, det_y = None, None
-            else:
-                det_x, det_y = result
-        else:
-            det_x, det_y = None, None
 
-        if self.state == "LOCKED" :
-            cx, cy, locked = self._update_locked(det_x, det_y)
-        else:
-            confirmed = self._update_lost(det_x, det_y) 
-            if confirmed:
-                cx = self.filter_x.x
-                cy = self.filter_y.x
-                locked = True
-            else:
-                cx = None
-                cy = None
-                locked = False
-
-        return cx, cy, locked
-            
+        self.cv_tracker = None
         
-
     def is_same_target(self, det_x, det_y, pred_x, pred_y, gate_radius):
         if self.distance(det_x, det_y, pred_x, pred_y) <= gate_radius:
             return True
@@ -54,9 +27,12 @@ class HybridBodyTracker:
     
     def distance(self, x1, y1, x2, y2):
         return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2))
+    
+    def _make_cv_tracker(self):
+        import cv2
+        return cv2.legacy.TrackerMOSSE_create()
 
-
-    def _update_lost(self, det_x, det_y):
+    def _update_lost(self, det_x, det_y, det_bbox, frame):
         if det_x is None:
             self.candidate_pos = None
             self.candidate_count = 0
@@ -76,12 +52,23 @@ class HybridBodyTracker:
             self.filter_x.init(det_x)
             self.filter_y.init(det_y)
             self.state = "LOCKED"
+            self.cv_tracker = self._make_cv_tracker()
+            self.cv_tracker.init(frame, det_bbox)
+            
             return True
         else:
             return False
 
-    def _update_locked(self, det_x, det_y):
+    def _update_locked(self, det_x, det_y, frame, det_bbox):
         locked = True
+
+        track_ok, tbox = self.cv_tracker.update(frame)
+        if track_ok:
+            tcx = tbox[0] + tbox[2] / 2
+            tcy = tbox[1] + tbox[3] / 2
+        else:
+            tcx, tcy = None, None
+
         self.filter_x.predict()
         self.filter_y.predict()
         pred_x = self.filter_x.x
@@ -90,7 +77,12 @@ class HybridBodyTracker:
         if det_x is not None and self.is_same_target(det_x, det_y, pred_x, pred_y, self.gate_radius):
             self.filter_x.correct(det_x)
             self.filter_y.correct(det_y)
+            self.cv_tracker = self._make_cv_tracker()
+            self.cv_tracker.init(frame, det_bbox)
             locked = True
+        elif det_x is None and track_ok:
+            self.filter_x.correct(tcx)
+            self.filter_y.correct(tcy)
         else:
             self.filter_x.misses += 1
             self.filter_y.misses += 1
@@ -104,6 +96,33 @@ class HybridBodyTracker:
         cx = self.filter_x.x
         cy = self.filter_y.x
     
+        return cx, cy, locked
+    
+    def update(self, frame):
+        self.frame_idx += 1
+        run_detection = (self.state !="LOCKED") or (self.frame_idx % self.detect_every_n == 0)
+        if run_detection:
+            result = self.detect_fn(frame)
+            if result is None:
+                det_x, det_y, det_bbox = None, None, None
+            else:
+                det_x, det_y, det_bbox = result
+        else:
+            det_x, det_y, det_bbox = None, None, None
+
+        if self.state == "LOCKED" :
+            cx, cy, locked = self._update_locked(det_x, det_y, frame, det_bbox)
+        else:
+            confirmed = self._update_lost(det_x, det_y, det_bbox, frame) 
+            if confirmed:
+                cx = self.filter_x.x
+                cy = self.filter_y.x
+                locked = True
+            else:
+                cx = None
+                cy = None
+                locked = False
+
         return cx, cy, locked
             
 
